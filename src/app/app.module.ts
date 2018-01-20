2eb17eb11b4e3e172ed613cc022ac1dbe390afe6
Gerry Mc Donnell <irishbloke@gmail.com>
2018-01-20 16:24:11 +0000

    added bootstrap navbar for sandbox1 and 2 router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {HttpModule} from '@angular/http'
+import {RouterModule,Routes} from '@angular/router'
 
 //import sandboxcompoent from components folder
 import { SandboxComponent } from './components/sandbox/sandbox.component';
@@ -11,17 +12,34 @@ import { SandboxComponent2 } from './components/sandbox2/sandbox2.component';
 //import Dataservice
 import { DataService } from './services/data.service';
 
+//create components
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+//create routes for app
+const appRoutes:Routes=[
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'sandbox1',component:SandboxComponent},
+  {path:'sandbox2',component:SandboxComponent2}
+]
+
 @NgModule({
   declarations: [    
     AppComponent,
     //import sandbox component
     SandboxComponent,
-    SandboxComponent2
+    SandboxComponent2,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   //services are added to providers
   providers: [DataService],
